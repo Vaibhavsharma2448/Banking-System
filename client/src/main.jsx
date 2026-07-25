0f75d0315800855debe3a517a1bf0ba5cccff9f0
Vaibhavsharma2448 <cse.vaibhav2000@gmail.com>
2026-07-25 10:58:09 +0530

    Fix duplicate React root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
